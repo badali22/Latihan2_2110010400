fe0c851455ba0c7d34f8772b93d2152efc1e3640
badali22 <badaliamin22@gmail.com>
2023-06-13 20:00:18 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,15 @@
+package universitas;
+
+public class Mahasiswa {
+
+    private final String npm;
+    private final String nama;
+    private final String prodi;
+    
+    public Mahasiswa (String npm, String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    
+}
